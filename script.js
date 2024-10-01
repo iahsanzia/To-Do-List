f5a0ca4fd9888b634995c3c3d94d4c943b3da89a
iahsanzia <99967291+iahsanzia@users.noreply.github.com>
2024-10-01 13:12:19 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const addButton = document.getElementById("addTask");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
+loadTask();
+
 function addTask(event){
     event.preventDefault();
     const task = taskInput.value.trim();
@@ -9,6 +11,7 @@ function addTask(event){
     if(task){
             createList(task);
             taskInput.value = '';
+            saveTask();
     }
 
     else{
@@ -20,6 +23,29 @@ addButton.addEventListener('click',addTask);
 function createList(task){
     const listItems = document.createElement('li');
     listItems.textContent = task;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteTask';
+
+    listItems.appendChild(deleteButton);
     taskList.appendChild(listItems);
-    console.log('Task added: ', task)
+
+    deleteButton.addEventListener('click', function(){
+        taskList.removeChild(listItems);
+        saveTask();
+    })
+}
+
+function saveTask(){
+    let tasks = [];
+    taskList.querySelectorAll('li').forEach(function(item){
+        tasks.push(item.textContent.replace('Delete','').trim());
+    });
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+
+function loadTask(){
+    const task = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    task.forEach(createList);
 }
